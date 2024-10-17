285c77f6cbc8a9174c1383d93cc26ae43e66a2fe
iftekhm91 <125846593+iftekhm91@users.noreply.github.com>
2024-10-17 12:53:25 +1100

    Update create-changeset.sh

--- Scripts/create-changeset.sh
+++ Scripts/create-changeset.sh
@@ -40,12 +40,41 @@ do
     shift
 done
 
+# Define environments where Dctm-Wm S3 and EFS stacks should be included
+WM_S3_DEPLOY_ENVIRONMENTS=("npd-dev" "npd-st" "ppd-customer")
+WM_EFS_DEPLOY_ENVIRONMENTS=("npd-dev" "npd-st" "ppd-customer")
+
+# S3 CHANGESET CREATION for Dctm-Wm
+if [[ " ${WM_S3_DEPLOY_ENVIRONMENTS[@]} " =~ " ${ENVIRONMENT_NAME} " ]]; then
+    # Create changeset for S3 Dctm-Wm
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-wm-s3" "$S3_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/s3-wm.properties" "$CFN_ROLE_ARN"
+fi
+
 # S3 CHANGESET CREATION for Dctm
 if [[ "$ENVIRONMENT_NAME" = npd-* ]]; then
     # NPD [ DEV - ST ] - INTERNAL
-    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-internal-s3" "$S3_TEMPLATE" "Infrastructure/Parameters/${ENVIRONMENT_NAME}/s3-internal.properties" 
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-internal-s3" "$S3_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/s3-internal.properties" "$CFN_ROLE_ARN"
     # NPD [ DEV - ST ] - CUSTOMER
-    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-customer-s3" "$S3_TEMPLATE" "Infrastructure/Parameters/${ENVIRONMENT_NAME}/s3-customer.properties" 
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-customer-s3" "$S3_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/s3-customer.properties" "$CFN_ROLE_ARN"
 else
-    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-s3" "$S3_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/s3.properties" 
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-s3" "$S3_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/s3.properties" "$CFN_ROLE_ARN"
+fi;
+
+# EFS CHANGESET CREATION for Dctm-Wm
+if [[ " ${WM_EFS_DEPLOY_ENVIRONMENTS[@]} " =~ " ${ENVIRONMENT_NAME} " ]]; then
+    # Create changeset for EFS Dctm-Wm
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-wm-efs" "$EFS_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/efs-wm.properties" "$CFN_ROLE_ARN"
+fi
+
+# EFS CHANGESET CREATION for Dctm
+if [[ "$ENVIRONMENT_NAME" = npd-* ]]; then
+    create_update_change_set "${APPLICATION_NAME}-npd-efs" "$EFS_TEMPLATE" "infrastructure/parameters/npd/efs.properties" "$CFN_ROLE_ARN"
+else
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-efs" "$EFS_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/efs.properties" "$CFN_ROLE_ARN"
+fi;
+
+# EFS CHANGESET CREATION for Dctm in ST
+if [[ "$ENVIRONMENT_NAME" = npd-st ]]; then
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-customer-efs" "$EFS_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/efs-customer.properties" "$CFN_ROLE_ARN"
+    create_update_change_set "${APPLICATION_NAME}-${ENVIRONMENT_NAME}-internal-efs" "$EFS_TEMPLATE" "infrastructure/parameters/${ENVIRONMENT_NAME}/efs-internal.properties" "$CFN_ROLE_ARN"
 fi;
